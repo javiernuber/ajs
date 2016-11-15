6f680100c9bfbb1130e57669389c998a34622939
Javier Núñez Berrocoso <javiernuber@gmail.com>
2016-11-15 20:49:46 +0100

    Form tests

---
+++ frontend/app/scripts/controllers/newUser.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict'
+
+    window.angular
+        .module('app')
+        .controller('NewUserController', controller)
+
+    function controller () {
+        var vm = this
+
+        vm.user = {
+            name: '',
+            surname: '',
+            birthdate: '',
+            department: 0,
+            job: '',
+            salary: '',
+            photo: '',
+            observations: ''
+        }
+
+        vm.send = function () {
+            console.log(JSON.stringify(vm.user))
+        }
+
+        vm.fileChange = function (file) {
+            var reader
+            if (file.files.length === 0) {
+                return
+            }
+            
+            reader = new window.FileReader()
+            reader.onloadend = function () {
+                vm.user.photo = reader.result
+            }
+
+            reader.readAsDataURL(file.files[0])
+        }
+    }
+}())
